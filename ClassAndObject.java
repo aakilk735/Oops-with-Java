dd606e0df6abdaa3c3ba49413a7ac00d5008599f
Mohammad Aakil <aakilk735@gmail.com>
2025-04-30 18:09:39 +0530

    Add files via upload

---
+++ ClassAndObject.java
@@ -0,0 +1,13 @@
+//java class
+class Father{
+    String son="Aakil Khan";
+    String daughter="Sofia Saifi";
+}
+public class ClassObject {
+    public static void main(String[] args) {
+        // creating object 
+        Father sc=new Father();
+        System.out.println(sc.son);
+        System.out.println(sc.daughter);
+    }
+}
