7c1fc51c1e1e1cf83b0190b0210478d8b690ff5f
Mohammad Aakil <aakilk735@gmail.com>
2025-04-30 18:14:52 +0530

    Add files via upload

--- Inheritance.java
+++ Inheritance.java
@@ -1,38 +1,93 @@
+// simple Inheritance
+/* class A{
+    public void PrintA(){
+        System.out.println("A");
+    }
+}
+class B extends A{
+    public void PrintB(){
+        System.out.println("B");
+    }
+}
+public class TestInheritence {
+    public static void main(String[] args) {
+        B sc = new B();
+        sc.PrintA();
+        sc.PrintB();
+    }
+}
+*/ //till here code of simple inheritance
 
-class Student{
-    private int Id;
-    private String name;
-    private String city;
-    public int getId(){
-        return Id;
+// Multi-level Inheritance
+
+/*
+class A{
+    public void printA(){
+        System.out.println("A");
     }
-    public void setId(int Id){
-        this.Id=Id;
+}
+class B extends A{
+    public void printB(){
+        System.out.println("B");
     }
-    public String getName(){
-        return name;
+}
+class C extends B{
+    public void printc(){
+        System.out.println("C");
+    }
+}
+class D extends C{
+    public void printD(){
+        System.out.println("D");
+    }
+}
+public class TestInheritence {
+    public static void main(String[] args) {
+        D sc=new D();
+        sc.printA();
+        sc.printB();
+        sc.printc();
+        sc.printD();
+    }
+}
+*/ // till here code of Multi-Inheritance
+
+// Multiple Inheritance(java does not support multiple inheritance but we can do with interface )
+
+/*
+interface Coder{
+    void writeCode();
+}
+interface Tester{
+    void testCode();
+}
+interface jdbc{
+    void connect();
+}
+class DevOps implements  Coder ,Tester,jdbc{
+    @Override
+    public void writeCode(){
+        System.out.println("i am writing code of Web Application");
+    }
+    @Override
+    public void testCode(){
+        System.out.println("i am testing of Web Application");
     }
 
-    public void setName(String name){
-        this.name=name;
-    }
-    public String getCity(){
-        return city;
-    }
-    public void setCity(String city){
-        this.city=city;
+    @Override
+    public void connect(){
+        System.out.println("it connection with jdbc in spring");
     }
 }
 
-public class Encapsulation {
+public class TestInheritence {
     public static void main(String[] args) {
 
-        Student sc=new Student();
-        sc.setId(1);
-        sc.setName("Mohammad Aakil");
-        sc.setCity("Bahorawas");
-        System.out.println("Id: "+sc.getId());
-        System.out.println("Name: "+sc.getName());
-        System.out.println("City: "+sc.getCity());
+        DevOps sc=new DevOps();
+        sc.testCode();
+        sc.writeCode();
+        sc.connect();
+
     }
 }
+*/
