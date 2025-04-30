ca2905a83ec13c76db3f111c1f53781dc626d006
Mohammad Aakil <aakilk735@gmail.com>
2025-04-30 18:44:38 +0530

    Add files via upload

---
+++ Abstraction(Interface).java
@@ -0,0 +1,16 @@
+interface Shape {
+    void draw();
+}
+class Rectangle implements Shape{
+    @Override
+    public void draw(){
+        System.out.println("drawing rectangle");
+}
+}
+//Main class to create object and call methods
+public class AbstractionWithInterface{A
+    public static void main(String args[]){
+        Rectangle sc =new Rectangle();
+        sc.draw();
+    }
+}
